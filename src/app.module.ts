3198d08de380b8f6bb6d263e1d10eb31d9629a5f
shakildevseoo <shakildevseoo@gmail.com>
2026-01-12 22:35:33 +0600

    add prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal : true}),
-    UserModule
+    UserModule,
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
