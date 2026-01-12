7226c2e249fbae3e7287c0a3480ad7165dd1d4fb
shakildevseoo <shakildevseoo@gmail.com>
2026-01-12 22:44:35 +0600

    prisma add successfully and (install maria-db for prisma problem)

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,25 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
 
+    constructor(private config : ConfigService){
+        const adapter = new PrismaMariaDb({
+            host :  config.get('DATABASE_HOST'),
+            user :  config.get('DATABASE_USER'),
+            port :  config.get('DATABASE_PORT'),
+            database : config.get('DATABASE_NAME'),
+            password : config.get('DATABASE_PASSWORD'),
+            connectionLimit : 5
+        })
+
+        super({
+            adapter
+        })
+    }
 
    async onModuleInit() {
        await this.$connect()
